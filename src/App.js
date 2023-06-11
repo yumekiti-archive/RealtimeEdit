abc81417a037dbd763af64bbf1ebb9123e821737
yumekiti <yumekiti120440@gmail.com>
2023-06-11 09:49:02 +0900

    `init`

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const ydocRef = useRef(null);
+  const yWebSocketProviderRef = useRef(null);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    ydocRef.current = new Y.Doc();
+
+    yWebSocketProviderRef.current = new WebsocketProvider(
+      'ws://localhost:1234',
+      'roomname',
+      ydocRef.current
+    );
+
+    const yText = ydocRef.current?.getText('textarea');
+    if (yText) {
+      const observer = () => {
+        setText(yText.toString());
+      };
+      yText.observe(observer);
+      return () => {
+        yText.unobserve(observer);
+      };
+    }
+
+    return () => {
+      ydocRef.current?.destroy();
+      yWebSocketProviderRef.current?.destroy();
+    };
+  }, []);
+
+  const handleTextareaChange = (e) => {
+    const yText = ydocRef.current?.getText('textarea');
+    if (yText) {
+      yText.delete(0, yText.length);
+      yText.insert(0, e.target.value);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        rows={10}
+        cols={50}
+        value={text}
+        onChange={handleTextareaChange}
+      />
     </div>
   );
 }
